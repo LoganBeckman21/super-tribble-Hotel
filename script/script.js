f7106715d8ad286f570340481a35f64231e0aa10
LoganBeckman21 <142538577+LoganBeckman21@users.noreply.github.com>
2023-10-31 20:21:10 -0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,15 @@
+$(function () {
+    //what radio value did they select?
+  
+    $("#formSubmit").on("click", (e) => {
+      //ii. Package the data
+      
+      e.preventDefault();
+      
+      alert(
+        
+      );
+    });
+  
+
+  });
